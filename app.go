73b700d57e346c42d7ee6d5e3be9acc1fc1ede1b
dylan0804 <dylan.halim@binus.ac.id>
2025-05-04 23:18:14 +0700

    feat: add context cancellation

--- app.go
+++ app.go
@@ -1,39 +1,50 @@
 package main
 
 import (
+	_ "embed"
+
 	"context"
 	"glimpse/screenshots"
 )
 
+//go:embed ocr-helper
+var ocrHelper []byte
+
 // App struct
 type App struct {
 	ctx context.Context
+	cancel context.CancelFunc
 	screenshotService screenshots.Service
-	ocr screenshots.OCRProvider
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
 	d := screenshots.NewDirProvider()
-	o := screenshots.NewOCRProvider()
+	o := screenshots.NewOCRProvider(ocrHelper)
+	i := screenshots.NewIndexer()
 
 	return &App{
-		screenshotService: screenshots.NewScreenshotService(d, o),
+		screenshotService: screenshots.NewScreenshotService(d, o, i),
 	}
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
-func (a *App) startup(ctx context.Context) {
+func (a *App) startup(ctx context.Context, cancel context.CancelFunc) {
 	a.ctx = ctx
+	a.cancel = cancel
 }
 
-// func (a *App) shutdown(ctx context.Context) {
-// 	a.ocr.Close()
-// }
+func (a *App) shutdown(_ context.Context) {
+	a.cancel()
+}
 
 func (a *App) ScanScreenshots() error {
-	a.screenshotService.ScanAndIndex()
+	err := a.screenshotService.ScanAndIndex(a.ctx)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
