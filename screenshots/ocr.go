d6efd9612b7b4f978119e6357adf23eadecbf3f5
dylan0804 <dylan.halim@binus.ac.id>
2025-05-04 23:17:28 +0700

    feat: add ocr helper binary

--- screenshots/ocr.go
+++ screenshots/ocr.go
@@ -1,25 +1,53 @@
 package screenshots
 
 import (
+	_ "embed"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 type OCRProvider interface {
 	ExtractText(path string) (string, error)
+	WriteOCRHelper() error
 }
 
-type OCR struct {}
+type OCR struct {
+	ocrBinary []byte
+	ocrBinaryPath string
+}
 
-func NewOCRProvider() OCRProvider {
-	return &OCR{}
+func NewOCRProvider(ocrBinary []byte) OCRProvider {
+	return &OCR{
+		ocrBinary: ocrBinary,
+	}
 }
 
 func (o *OCR) ExtractText(path string) (string, error) {
-	out, err := exec.Command("../ocr-helper/ocr-helper", path).Output()
+	out, err := exec.Command(o.ocrBinaryPath, path).Output()
 	if err != nil {
 		return "", err
 	}
 
 	return strings.TrimSpace(string(out)), nil
+}
+
+func (o *OCR) WriteOCRHelper() error {
+	tmpFile, err := os.CreateTemp("", "ocr-helper-*")
+	if err != nil {
+		return err
+	}
+	defer tmpFile.Close()
+
+	if _, err := tmpFile.Write(o.ocrBinary); err != nil {
+		return err
+	}
+
+	if err := os.Chmod(tmpFile.Name(), 0755); err != nil {
+		return err
+	}
+
+	o.ocrBinaryPath = tmpFile.Name()
+
+	return nil
 }
